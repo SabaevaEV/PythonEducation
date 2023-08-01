faaf8f0688b928d0381ba966769a5331384f5b22
SabaevaEvgeniya <eva230391@mail.ru>
2023-08-01 16:01:33 +0400

    added folders with information

---
+++ lections/main.py
@@ -0,0 +1,56 @@
+print (5)
+n = 5 # // int n = 5;
+n = None
+n = 1.89
+n =  'dhehw'
+n = "fjejhf"
+"Способ ввода/вывода данных"
+"""
+print (type(n)) # Вывод ТИПА данных
+n = 'sdgsg\'cxhb' # выведется ' в тексте из-за \
+n = 'sdgsg"gijefu"cxhb' # выведется "" в тексте
+"""
+
+a = 5
+b = 5.89
+c = "hello'"
+print (a,b,c)
+print (a,' - ',b, '-',c)
+print (f"{a} - {b}")
+print ("{} - {} - {}" .format(a,b,c))
+
+
+input () # функция ввода данных
+print ('введите число')
+a = input()
+print (a)
+b = input ("Введите второе число: ")# можно выводить так
+
+print (a, '+', b, '=', a+b)
+
+c=5.89
+print(c)
+print(type (c))
+n = int(c) # так можно менять ТИП данных
+print (n)
+
+print ('введите число')
+a = int(input())
+print (a)
+b = int (input ("Введите второе число: "))
+print (a, '+', b, '=', a+b)
+
+
+a = 5.624
+b = 6.24856
+print(round (a*b), 3)
+
+iter = 2
+iter +=3 # iter = iter + 3
+iter -= 3 # iter = iter + 3
+iter *= 3 # iter = iter + 3
+iter /= 3 # iter = iter + 3
+iter //= 3 # iter = iter + 3
+iter %= 3 # iter = iter + 3
+iter **= 3 # iter = iter + 3
+
